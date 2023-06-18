333d39a02d5ab67eaaf8c5922161bd5e6847e7c7
Liam Wood <general.lvwood@gmail.com>
2023-06-18 11:33:38 -0500

    Update build.sh to current os or optionally linux

--- build.sh
+++ build.sh
@@ -1,2 +1,35 @@
-docker build -t core-engine ./buildenv
-docker run --rm -v "$PWD":/src -w /usr/src/ gcc:4.9 make
+#!/bin/bash
+
+function build_linux()
+{
+    docker build -t core-engine ./buildenv
+    docker run --rm -v "$PWD":/usr/core core-engine make
+}
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -tp|--target-platforms)
+            TARGETS="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Check if target architectures are specified and call the appropriate build function
+if [[ -n "${TARGETS[*]}" ]]; then
+    if [[ "${TARGETS[*]}" == *"Linux"* ]]; then
+        build_linux "${TARGETS[*]}"
+    else
+        echo "Unsupported target architecture: ${TARGETS[*]}"
+        exit 1
+    fi
+else
+    make
+fi
