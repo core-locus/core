211143c9b67dcae5453764511d209baf082a94da
Liam Wood <general.lvwood@gmail.com>
2023-08-27 10:30:36 -0500

    Setup entrypoint for core/cacus engine

---
+++ src/Cacus/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+namespace Cacus
+{
+    Application::Application()
+    {
+    }
+
+    Application::~Application()
+    {
+    }
+
+    void Application::Run()
+    {
+        while (true);
+    }
+} // namespace Cacus
