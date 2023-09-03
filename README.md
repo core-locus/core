faae115d24ddc1857945a2b3bb05f12447c676ac
Liam Wood <general.lvwood@gmail.com>
2023-09-02 20:49:20 -0400

    Add premake build system

--- README.md
+++ README.md
@@ -2,27 +2,17 @@
 core game engine for upcoming voxel based game
 
 # Building
-We currently support building for Linux and MacOS with plans to add support for windows (as soon as we figure out how)
+We are using the (premake)[https://github.com/premake/premake-core/wiki/] build system to target development on Windows, MacOS and Linux
 
-For now, you can target the current platform with:
+To generate build files on MacOS run from project root:
 ```
-$ ./build.sh
-```
-
-If you would also like to output for the current executable use:
-```
-$ ./build-and-run.sh
+./vendor/bin/premake/premake5 gmake2 && make
 ```
 
-For Linux:
+You can run the sample project with:
 ```
-$ ./build.sh (-tp|--target-platforms) Linux
+./bin/Debug-macosx-x86_64/Sandbox/Sandbox
 ```
-and to run:
-```
-$ ./build-and-run.sh (-tp|--target-platforms) Linux
-```
-
 
 # References
 https://hub.docker.com/_/gcc/
---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Cacus"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Cacus"
+    location "src"
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++11"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/%{prj.name}/**.h",
+        "src/%{prj.name}/**.cpp"
+    }
+
+    includedirs
+    {
+        "src/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CCS_PLATFORM_WINDOWS",
+            "CCS_BUILD_DLL"
+        }
+
+    filter "system:macos"
+        staticruntime "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "CCS_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CCS_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CCS_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "examples/%{prj.name}"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++11"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "examples/%{prj.name}/src/**.h",
+        "examples/%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "src/vendor/spdlog/include",
+        "src"
+    }
+
+    links
+    {
+        "Cacus"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CCS_PLATFORM_WINDOWS"
+        }
+
+    filter "system:macos"
+        staticruntime "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "CCS_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CCS_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CCS_DIST"
+        optimize "On"
